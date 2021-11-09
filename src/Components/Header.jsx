53342f781d3343e35c69893319a83616a0fb3f95
jaeger-dvlp <omer.kayalar0101@icloud.com>
2021-11-09 10:12:29 +0300

    Increase size of text

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 export default function Header() {
   return (
-    <div className="header text-white main-bg-grad py-5 fixed font-pop font-semibold flex w-full flex-wrap text-center justify-center">
-      🕵️ AnonWrites
+    <div className="header text-white main-bg-grad py-5 fixed font-pop font-medium flex w-full flex-wrap text-center justify-center text-2xl">
+      AnonWrites
       <span className="header-anim">_</span>
     </div>
   );
