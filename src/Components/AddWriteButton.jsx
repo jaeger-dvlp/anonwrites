145b1a0815477b8b320bc3cee4506a78d95a07dc
jaeger-dvlp <omer.kayalar0101@icloud.com>
2021-11-11 10:50:21 +0300

    Create plus button

---
+++ src/Components/AddWriteButton.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function AddWriteButton() {
+  return (
+    <div className="container fixed mx-auto">
+      <div className="addWriteButton fixed right-10 bottom-10 flex justify-center content-center p-0 py-0 px-0 rounded-xl text-3xl bg-gray-800 text-white">
+        +
+      </div>
+    </div>
+  );
+}
