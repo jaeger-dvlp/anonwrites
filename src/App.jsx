89c0b10cb3d24b2e930bfccb7b9dbb912bc50e0f
jaeger-dvlp <omer.kayalar0101@icloud.com>
2021-11-11 10:48:40 +0300

    Import aos & init

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 // eslint-disable-next-line import/named
 import { ContextProvider } from './Context';
 import Header from './Components/Header';
 import Homepage from './Components/Homepage';
+import AddWriteButton from './Components/AddWriteButton';
 
+AOS.init();
 function App() {
   return (
     <ContextProvider>
       <Header />
       <Homepage />
+      <AddWriteButton />
     </ContextProvider>
   );
 }
