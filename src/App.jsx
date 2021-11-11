f5ec007314f2b3fa9f7c86f3c9efad35c873a20f
jaeger-dvlp <omer.kayalar0101@icloud.com>
2021-11-11 15:44:21 +0300

    Import router & component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 // eslint-disable-next-line import/named
@@ -6,13 +7,21 @@ import { ContextProvider } from './Context';
 import Header from './Components/Header';
 import Homepage from './Components/Homepage';
 import AddWriteButton from './Components/AddWriteButton';
+import CategoryWrite from './Components/CategoryWrite';
+import NotFound from './Components/NotFound';
 
 AOS.init();
 function App() {
   return (
     <ContextProvider>
       <Header />
-      <Homepage />
+      <Router>
+        <Switch>
+          <Route path="/" exact component={Homepage} />
+          <Route path="/category/:name" component={CategoryWrite} />
+          <Route component={NotFound} />
+        </Switch>
+      </Router>
       <AddWriteButton />
     </ContextProvider>
   );
