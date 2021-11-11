bb9f88fc85ae95974bf1fb290b3c5662f0aa96ce
jaeger-dvlp <omer.kayalar0101@icloud.com>
2021-11-11 15:44:21 +0300

    Create not found component

---
+++ src/Components/NotFound.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function NotFound() {
+  return (
+    <div className="pt-20 w-full justify-center text-white">
+      Looks like you lost.
+    </div>
+  );
+}
