f771a29f917525e3df156c1b19b5e749f47486e2
jaeger-dvlp <omer.kayalar0101@icloud.com>
2021-11-12 10:20:34 +0300

    Add fade

--- src/Components/LoadingWrite.jsx
+++ src/Components/LoadingWrite.jsx
@@ -2,7 +2,12 @@ import React from 'react';
 
 export default function LoadingWrite() {
   return (
-    <div className="w-full transition-all duration-300 justify-center flex flex-wrap">
+    <div
+      data-aos="fade-in"
+      data-aos-duration="1000"
+      data-aos-delay="300"
+      className="w-full transition-all duration-300 justify-center flex flex-wrap"
+    >
       <div className="write m-2 my-10 xl:w-1/2 lg:w-1/2 flex flex-wrap overflow-hidden justify-center content-start rounded-xl md:w-1/2 w-full bg-gray-800 p-0">
         <div className="write-header w-full flex flex-wrap border-b border-gray-900 p-5">
           <div className="w-1/3 h-full bg-gray-700 rounded-lg p-5 animate-pulse " />
