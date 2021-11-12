43411773dbcca3fa41f8d416e85e004b80486d1d
jaeger-dvlp <omer.kayalar0101@icloud.com>
2021-11-12 17:25:21 +0300

    Add ainmation

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Header() {
   return (
-    <div className="header z-50 text-white main-bg-grad py-5 fixed font-pop font-medium flex w-full flex-wrap text-center justify-center text-2xl">
-      AnonWrites
-      <span className="header-anim">_</span>
+    <div
+      data-aos="fade-down"
+      data-aos-duration="1000"
+      data-aos-delay="300"
+      className="header z-50 text-white main-bg-grad py-5 fixed font-pop font-medium flex w-full flex-wrap text-center justify-center text-2xl"
+    >
+      <Link to="/">
+        AnonWrites
+        <span className="header-anim">_</span>
+      </Link>
     </div>
   );
 }
