149b7816b9e8d275c37a1ab9cb2695f6b2c1ba0a
jaeger-dvlp <omer.kayalar0101@icloud.com>
2021-11-15 13:58:07 +0300

    Change popup && map data

--- src/Components/CategoryWrite.jsx
+++ src/Components/CategoryWrite.jsx
@@ -48,7 +48,7 @@ export default function CategoryWrite({ history }) {
       })
         .then((res) => res.json())
         .then((data) => {
-          if (data.includes(categoryName) !== true) {
+          if (data[0].categories.includes(categoryName) !== true) {
             setCatStatus(true);
           } else {
             sendWritesByCategory();
@@ -61,28 +61,40 @@ export default function CategoryWrite({ history }) {
 
   const notFound = (
     <div
-      data-aos="fade-down"
-      data-aos-delay="200"
-      data-aos-duration="700"
-      className="w-full flex text-center flex-wrap justify-center content-center font-pop text-gray-300 p-3 m-0"
+      className={`w-full font-pop ${
+        catStatus === true ? 'opacity-1 visible' : 'opacity-0 invisible'
+      } flex flex-wrap justify-center content-center transition-all duration-300 h-full fixed top-0 left-0 bg-black bg-opacity-50`}
     >
-      <div className="write m-2 xl:w-1/2 lg:w-1/2 md:w-1/2 w-full flex flex-wrap overflow-hidden justify-center content-start rounded-xl bg-gray-800 p-0">
-        <div className="write-content w-full p-0">
-          <div className="w-full text-center flex justify-center content-center p-5 border-b border-gray-900">
-            <span className="p-2 px-5 overflow-hidden shadow-lg text-xl fill-current flex flex-wrap rounded-md justify-center content-center bg-red-500 text-red-500 bg-opacity-40">
-              Not Found
-            </span>
-          </div>
-          <div className="w-full justify-center content-center flex flex-wrap p-5">
-            <span className="w-full text-center text-2xl py-5">Oops.</span>
-            <span className="w-full text-center text-lg pb-5 font-light">
+      <div className="container mx-auto p-0 m-0">
+        <div className="w-full flex flex-wrap justify-center content-center p-5">
+          <div
+            data-aos="fade-down"
+            className="xl:w-1/3 lg:w-1/2 w-full bg-gray-800 rounded-xl text-white p-0 shadow-xl"
+          >
+            <div className="popupHeader w-full flex flex-wrap justify-center content-center p-4 border-b border-gray-900">
+              <div className="p-3 bg-red-500 fill-current text-red-500 bg-opacity-30 rounded-full">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm6 16.538l-4.592-4.548 4.546-4.587-1.416-1.403-4.545 4.589-4.588-4.543-1.405 1.405 4.593 4.552-4.547 4.592 1.405 1.405 4.555-4.596 4.591 4.55 1.403-1.416z" />
+                </svg>
+              </div>
+            </div>
+            <div className="w-full text-center flex flex-wrap justify-center content-center p-5">
               Looks like this category is doesn&apos;t exist.
-            </span>
-          </div>
-          <div className="w-full text-lg  border-t border-gray-900 text-center flex flex-wrap justify-center content-center p-7">
-            <Link className="text-gray-400 hover:text-gray-200" to="/">
-              Back
-            </Link>
+            </div>
+            <div className="w-full flex flex-wrap justify-center content-center p-5">
+              <Link
+                to="/"
+                className="px-5 py-2 hover:bg-opacity-60 rounded-lg bg-gray-500 text-gray-300 bg-opacity-30"
+                type="button"
+              >
+                Okay.
+              </Link>
+            </div>
           </div>
         </div>
       </div>
