b33c5ac4602d21ed4441f65f72ba4d5f9d03a8ee
jaeger-dvlp <omer.kayalar0101@icloud.com>
2021-11-17 13:45:49 +0300

    Create landing sect

--- src/Components/Homepage.jsx
+++ src/Components/Homepage.jsx
@@ -12,13 +12,28 @@ export default function Homepage() {
 
   const writeElms = writeData
     ? writeData.map((elm) => (
-        <Write
-          key={faker.datatype.uuid()}
-          author={elm.writeAuthor}
-          content={elm.writeContent}
-          time={elm.writeTime}
-          categories={elm.writeCategories}
-        />
+        <>
+          <div
+            data-aos="fade-in"
+            data-aos-delay="100"
+            data-aos-duration="450"
+            className="conainer mb-10 mx-auto font-pop text-lg xl:w-1/2 lg:w-1/2 md:w-1/2 w-full text-left text-gray-400"
+          >
+            <div className="w-full pb-2 ">Hey, wanna join to mayhem?</div>
+            <div className="w-full text-sm rounded-lg bg-gray-800 flex xl:flex-nowrap lg:flex-nowrap flex-wrap xl:justify-start lg:justify-start justify-center content-center p-5">
+              <div className="self-center text-center">
+                Explore anonym articles around the world or create one yourself.
+              </div>
+            </div>
+          </div>
+          <Write
+            key={faker.datatype.uuid()}
+            author={elm.writeAuthor}
+            content={elm.writeContent}
+            time={elm.writeTime}
+            categories={elm.writeCategories}
+          />
+        </>
       ))
     : '';
 
