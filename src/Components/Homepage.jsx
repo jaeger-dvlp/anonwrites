3de976bf05183ddb0ca1a7a772a5ee52a6af2347
jaeger-dvlp <omer.kayalar0101@icloud.com>
2021-11-17 13:51:15 +0300

    Add landing sect

--- src/Components/Homepage.jsx
+++ src/Components/Homepage.jsx
@@ -13,19 +13,6 @@ export default function Homepage() {
   const writeElms = writeData
     ? writeData.map((elm) => (
         <>
-          <div
-            data-aos="fade-in"
-            data-aos-delay="100"
-            data-aos-duration="450"
-            className="conainer mb-10 mx-auto font-pop text-lg xl:w-1/2 lg:w-1/2 md:w-1/2 w-full text-left text-gray-400"
-          >
-            <div className="w-full pb-2 ">Hey, wanna join to mayhem?</div>
-            <div className="w-full text-sm rounded-lg bg-gray-800 flex xl:flex-nowrap lg:flex-nowrap flex-wrap xl:justify-start lg:justify-start justify-center content-center p-5">
-              <div className="self-center text-center">
-                Explore anonym articles around the world or create one yourself.
-              </div>
-            </div>
-          </div>
           <Write
             key={faker.datatype.uuid()}
             author={elm.writeAuthor}
@@ -40,7 +27,27 @@ export default function Homepage() {
   return (
     <div className="container mx-auto py-32 px-5">
       <div className="w-full text-white flex flex-wrap justify-center content-start">
-        {writeElms ? [writeElms] : <LoadingWrite />}
+        {writeElms ? (
+          <>
+            <div
+              data-aos="fade-in"
+              data-aos-delay="100"
+              data-aos-duration="450"
+              className="conainer mb-10 mx-auto font-pop text-lg xl:w-1/2 lg:w-1/2 md:w-1/2 w-full text-left text-gray-400"
+            >
+              <div className="w-full pb-2 ">Hey, wanna join to mayhem?</div>
+              <div className="w-full text-sm rounded-lg bg-gray-800 flex xl:flex-nowrap lg:flex-nowrap flex-wrap xl:justify-start lg:justify-start justify-center content-center p-5">
+                <div className="self-center text-center">
+                  Explore anonym articles around the world or create one
+                  yourself.
+                </div>
+              </div>
+            </div>
+            {[writeElms]}
+          </>
+        ) : (
+          <LoadingWrite />
+        )}
       </div>
     </div>
   );
